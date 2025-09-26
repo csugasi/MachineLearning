d5066324f74cf69c3f551557cd473ddeca3918d3
csugasi <161510222+csugasi@users.noreply.github.com>
2025-09-26 13:01:06 -0700

    Create utils_common.py
    Contains common routines and variable definitions

---
+++ AdvancedLearningAlgorithms/utils_common.py
@@ -0,0 +1,27 @@
+"""
+utils_common
+   contains common routines and variable definitions
+"""
+import copy
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+def sigmoid(z):
+    """
+    Compute the sigmoid of z
+
+    Parameters
+    ----------
+    z : array_like
+        A scalar or numpy array of any size.
+
+    Returns
+    -------
+     g : array_like
+         sigmoid(z)
+    """
+    z = np.clip( z, -500, 500 )           # protect against overflow
+    g = 1.0/(1.0+np.exp(-z))
+
+    return g
